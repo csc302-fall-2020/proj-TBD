3e9e36641204b8828f52c2c781a11f13e65d3176
icehunt <m@tom.ac>
2020-10-12 21:21:29 -0400

    Got flask mapped, still need to do a full api test

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+import datetime
+import os
+
+from flask import Flask, Response, request
+from flask_mongoengine import MongoEngine
+
+app = Flask(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    'host': os.environ['MONGODB_HOST'],
+    'username': os.environ['MONGODB_USERNAME'],
+    'password': os.environ['MONGODB_PASSWORD'],
+    'db': 'nametbd'
+}
+
+db = MongoEngine()
+db.init_app(app)
+
--- bin/build-dockers.sh
+++ bin/build-dockers.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 docker build -f frontend-docker -t name-tbd:frontend .
+docker build -f backend-docker -t name-tbd:backend .
