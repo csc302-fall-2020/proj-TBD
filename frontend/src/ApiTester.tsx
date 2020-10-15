022f2c13268489385a0421de5d2896880749939b
Matthew Tory <matthew.tory@mail.utoronto.ca>
2020-10-15 15:43:58 -0400

    Address PR comments
    • Reformat with prettier
    • Fix react router bug
    • Replace inline style with styled-components

--- frontend/src/ApiTester.tsx
+++ frontend/src/ApiTester.tsx
@@ -1,21 +1,44 @@
-import {Col, Layout, Radio, Row, Typography, Button, Space, Input, Spin, Alert} from 'antd';
-import React, {useState} from 'react';
-import axios, {AxiosError} from 'axios'
+import { Col, Layout, Radio, Row, Typography, Button, Space, Input, Spin, Alert } from 'antd';
+import React, { useState } from 'react';
+import axios, { AxiosError } from 'axios';
+import styled from 'styled-components';
 
-const {Content} = Layout
-const {Text} = Typography
-const {TextArea} = Input;
+const { Content } = Layout;
+const { Text } = Typography;
+const { TextArea } = Input;
 
 type ApiResponse = {
-    data: string,
-    error?: string
-}
+    data: string;
+    error?: string;
+};
 
-export type ApiTesterProps = { defaultRoute?: string }
+export type ApiTesterProps = { defaultRoute?: string };
 
-const ApiTester: React.FC<ApiTesterProps> = ({defaultRoute}) => {
-    const [route, setRoute] = useState(defaultRoute)
-    const [requestMode, setRequestMode] = useState<'get' | 'post'>('get')
+const LayoutFullHeight = styled(Layout)`
+    height: 100vh;
+    overflow: auto;
+`;
+
+const RowWithMargin = styled(Row)`
+    margin: 32px;
+`;
+
+const ColAsCard = styled(Col)`
+    background: #fff;
+    padding: 16px;
+`;
+
+const SpaceFullWidth = styled(Space)`
+    width: 100%;
+`;
+
+const InputFixedWidth = styled(Input)`
+    max-width: 200px;
+`;
+
+const ApiTester: React.FC<ApiTesterProps> = ({ defaultRoute }) => {
+    const [route, setRoute] = useState(defaultRoute);
+    const [requestMode, setRequestMode] = useState<'get' | 'post'>('get');
     const [requestBody, setRequestBody] = useState('{}');
     const [response, setResponse] = useState<ApiResponse | undefined>(undefined);
     const [loading, setLoading] = useState(false);
@@ -24,29 +47,28 @@ const ApiTester: React.FC<ApiTesterProps> = ({defaultRoute}) => {
         if (loading) return;
 
         setLoading(true);
-        const url = `/${route}`
-        let data = ''
-        let error: string | undefined
+        const url = `/${route}`;
+        let data = '';
+        let error: string | undefined;
         try {
             const response = await axios(url, {
                 method: requestMode,
                 headers: {
-                    'content-type': 'application/json'
+                    'content-type': 'application/json',
                 },
-                data: requestMode === 'post' ? JSON.parse(requestBody) : undefined
-            })
+                data: requestMode === 'post' ? JSON.parse(requestBody) : undefined,
+            });
 
             if (response.headers['content-type'] === 'application/json') {
-                data = JSON.stringify(response.data, undefined, '  ')
+                data = JSON.stringify(response.data, undefined, '  ');
             } else {
                 data = `${response.data}`;
             }
-
         } catch (e) {
             if (e.isAxiosError) {
                 const axiosError = e as AxiosError;
                 data = JSON.stringify(axiosError.toJSON(), undefined, '  ');
-                error = axiosError.message
+                error = axiosError.message;
             } else {
                 error = `${e}`;
             }
@@ -54,27 +76,27 @@ const ApiTester: React.FC<ApiTesterProps> = ({defaultRoute}) => {
             setLoading(false);
             setResponse({
                 data,
-                error
-            })
+                error,
+            });
         }
-    }
+    };
 
     return (
-        <Layout style={{height: '100vh', overflow: 'auto'}}>
+        <LayoutFullHeight>
             <Content>
-                <Row justify='center' style={{margin: '32px 32px'}}>
-                    <Col xs={12} style={{backgroundColor: '#fff', padding: '16px'}}>
+                <RowWithMargin justify="center">
+                    <ColAsCard xs={12}>
                         <h3>Route Tester</h3>
-                        <Space direction={'vertical'} style={{width: '100%'}}>
+                        <SpaceFullWidth direction="vertical">
                             <Row>
-                                <Col flex='auto'>
-                                    <Space size='small'>
-                                        {<Text type='secondary'>/</Text>}
-                                        <Input
-                                            type='text'
-                                            style={{maxWidth: '200px'}}
+                                <Col flex="auto">
+                                    <Space size="small">
+                                        {<Text type="secondary">/</Text>}
+                                        <InputFixedWidth
+                                            type="text"
                                             value={route}
-                                            onChange={(e) => setRoute(e.target.value)}/>
+                                            onChange={(e) => setRoute(e.target.value)}
+                                        />
                                     </Space>
                                 </Col>
                                 <Col>
@@ -82,37 +104,46 @@ const ApiTester: React.FC<ApiTesterProps> = ({defaultRoute}) => {
                                         <Radio.Group
                                             value={requestMode}
                                             onChange={(e) => {
-                                                setRequestMode(e.target.value)
-                                                setResponse(undefined)
-                                            }}>
-                                            <Radio.Button value='get'>GET</Radio.Button>
-                                            <Radio.Button value='post'>POST</Radio.Button>
-
+                                                setRequestMode(e.target.value);
+                                                setResponse(undefined);
+                                            }}
+                                        >
+                                            <Radio.Button value="get">GET</Radio.Button>
+                                            <Radio.Button value="post">POST</Radio.Button>
                                         </Radio.Group>
-                                        {loading ? <Spin/> : <Button type='primary' onClick={() => makeRequest()}>
-                                            Go
-                                        </Button>}
+                                        {loading ? (
+                                            <Spin />
+                                        ) : (
+                                            <Button type="primary" onClick={() => makeRequest()}>
+                                                Go
+                                            </Button>
+                                        )}
                                     </Space>
                                 </Col>
                             </Row>
-                            {requestMode === 'post' && <div>
-                                <h4>Body</h4>
-                                <TextArea
-                                    rows={5}
-                                    autoSize
-                                    value={requestBody}
-                                    onChange={(e) => setRequestBody(e.target.value)}/>
-                            </div>}
-                            {!loading && !!response && <>
-                                {!!response.error && <Alert message={response.error} type={'error'}/>}
-                                {!!response.data && <TextArea value={response.data} autoSize disabled/>}
-                            </>}
-                        </Space>
-                    </Col>
-                </Row>
+                            {requestMode === 'post' && (
+                                <div>
+                                    <h4>Body</h4>
+                                    <TextArea
+                                        rows={5}
+                                        autoSize
+                                        value={requestBody}
+                                        onChange={(e) => setRequestBody(e.target.value)}
+                                    />
+                                </div>
+                            )}
+                            {!loading && !!response && (
+                                <>
+                                    {!!response.error && <Alert message={response.error} type={'error'} />}
+                                    {!!response.data && <TextArea value={response.data} autoSize disabled />}
+                                </>
+                            )}
+                        </SpaceFullWidth>
+                    </ColAsCard>
+                </RowWithMargin>
             </Content>
-        </Layout>
+        </LayoutFullHeight>
     );
-}
+};
 
-export default ApiTester
+export default ApiTester;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,35 @@
 import ApiTester from 'ApiTester';
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
-    return (<HashRouter>
-        <Route path='/test'>
-            <ApiTester defaultRoute='test' />
-        </Route>
-        <Route path='/'>
-            <div className="App">
-                <header className="App-header">
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                        Learn React
-                </a>
-                </header>
-            </div>
-        </Route>
-    </HashRouter>);
+    return (
+        <HashRouter>
+            <Switch>
+                <Route path="/test">
+                    <ApiTester defaultRoute="test" />
+                </Route>
+                <Route path="/">
+                    <div className="App">
+                        <header className="App-header">
+                            <p>
+                                Edit <code>src/App.tsx</code> and save to reload.
+                            </p>
+                            <a
+                                className="App-link"
+                                href="https://reactjs.org"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Learn React
+                            </a>
+                        </header>
+                    </div>
+                </Route>
+            </Switch>
+        </HashRouter>
+    );
 }
 
 export default App;
