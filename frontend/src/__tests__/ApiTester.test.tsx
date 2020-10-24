35eaccace3116763258259123b4c95ae51fc49c9
christochow <christochow@yahoo.com.hk>
2020-10-24 18:42:34 +0800

    format ApiTester.test.tsx with prettier fix import to get rid of warning

--- frontend/src/__tests__/ApiTester.test.tsx
+++ frontend/src/__tests__/ApiTester.test.tsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import { render, wait, cleanup, screen, fireEvent } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
+import React from 'react';
+import { render, waitFor, cleanup, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import 'window';
 import ApiTester from '../ApiTester';
-import { rest } from 'msw'
-import { setupServer } from 'msw/node'
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 
 const server = setupServer(
     rest.get('/test', (req, res, ctx) => {
-        return res(ctx.text('hello there'))
+        return res(ctx.text('hello there'));
     })
-)
+);
 
-beforeAll(() => server.listen())
+beforeAll(() => server.listen());
 afterEach(() => {
     server.resetHandlers();
     cleanup();
-})
-afterAll(() => server.close())
+});
+afterAll(() => server.close());
 
 test('Renders title', async () => {
     const testMessage = 'Route Tester';
-    const { getByText } = render(<ApiTester defaultRoute="test" />)
+    const { getByText } = render(<ApiTester defaultRoute="test" />);
 
     expect(getByText(testMessage)).toBeInTheDocument();
-})
+});
 
 test('displays result on GET call', async () => {
     const returnMsg = 'hello there';
-    const route = "test";
-    const { getByText } = render(<ApiTester defaultRoute={route} />)
+    const route = 'test';
+    render(<ApiTester defaultRoute={route} />);
 
-    expect(screen.queryByText(returnMsg)).not.toBeInTheDocument()
-    fireEvent.click(getByText("Go"));
-    await wait(() => expect(getByText(returnMsg)).toBeInTheDocument());
-})
+    expect(screen.queryByText(returnMsg)).not.toBeInTheDocument();
+    fireEvent.click(screen.getByText('Go'));
+    await waitFor(() => screen.getByText(returnMsg));
+});
