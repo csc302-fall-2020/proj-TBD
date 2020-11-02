3e05b31f166a033fca5953d63616ca3a30c7ab25
christochow <christochow@yahoo.com.hk>
2020-11-02 14:19:54 +0800

    add env environments to get mongo working

--- bin/start-docker.sh
+++ bin/start-docker.sh
@@ -4,6 +4,9 @@ export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $SCRIPT_DIR
 export CODE_DIR=`dirname $SCRIPT_DIR`
 export HOME_DIR=$HOME
+export MONGODB_USERNAME="nametbd"
+export MONGODB_HOST="passtbd"
+export MONGODB_DB="SDC"
 compose_cmd="docker-compose -p $USER -f docker-compose.yaml"
 $compose_cmd up -d
 
