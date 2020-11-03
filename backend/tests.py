6cbb41d8728f97fed463ffd5aea20f8ad70f4f2d
Tim Cosby <tim.cosby@mail.utoronto.ca>
2020-11-02 20:13:09 -0500

    Improved testing.

--- backend/tests.py
+++ backend/tests.py
@@ -8,7 +8,7 @@ from werkzeug import exceptions
 
 class TestDBConnection(unittest.TestCase):
     APP = Flask(__name__)
-    APP.config['MONGO_URI'] = 'mongodb+srv://{username}:{password}@{host}/{db}?retryWrites=true&w=majority'.format(
+    APP.config['MONGO_URI'] = 'mongodb://{username}:{password}@{host}/{db}?retryWrites=true&w=majority'.format(
         username=os.environ['MONGODB_USERNAME'], password=os.environ['MONGODB_PASSWORD'],
         host=os.environ['MONGODB_HOST'], db=os.environ['MONGODB_DB'])
 
@@ -20,26 +20,31 @@ class TestDBConnection(unittest.TestCase):
 
         self.assertTrue(True)
 
-    def test_collections_exit(self):
+    def test_collections_exist(self):
         CLUSTER = PyMongo(self.APP)
 
         DB = CLUSTER.db
-        FORM_TABLE = DB.forms
-        FORM_RESPONSE_TABLE = DB.form_responses
 
-        self.assertTrue(FORM_TABLE is not None and FORM_RESPONSE_TABLE is not None)
+        self.assertTrue(DB is not None)
 
 
 class TestDBQueries(unittest.TestCase):
     context = app.APP.app_context()
     context.push()
 
+    app.DB.createCollection('forms')
+
+    app.FORM_TABLE.remove({'FormID': '-2'})
+    app.FORM_TABLE.insert_one({'FormName': 'Test1', 'FormID': '-2', 'Version': '1.0'})
+    app.FORM_TABLE.insert_one({'FormName': 'Test2', 'FormID': '-2', 'Version': '1.1'})
+    app.FORM_TABLE.insert_one({'FormName': 'Test3', 'FormID': '-2', 'Version': '1.13'})
+
     def test_get_form(self):
-        json, response_code = app.get_form(0)
+        json, response_code = app.get_form('-2')
 
         json = json.json
 
-        self.assertEqual(json['FormName'], 'Test1')
+        self.assertEqual(json['FormName'], 'Test3')
         self.assertEqual(response_code, 200)
 
     def test_invalid_form(self):
@@ -50,9 +55,10 @@ class TestDBQueries(unittest.TestCase):
         # In the database there are multiple versions for FormID: 0
         # Versions: 1.0, 1.1, 1.13
 
-        json, response_code = app.get_form(0)
+        json, response_code = app.get_form('-2')
 
         json = json.json
 
+        self.assertEqual(json['FormName'], 'Test3')
         self.assertEqual(json['Version'], '1.13')
         self.assertEqual(response_code, 200)
