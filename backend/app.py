e5a9e71254c1dae6c0f521ef158c308e460cfb57
Lucas <lucas@Lucass-iMac.local>
2020-11-11 17:31:42 -0500

    returning form meta data after form creation or patch

--- backend/app.py
+++ backend/app.py
@@ -378,7 +378,8 @@ def create_form():
     if request.method == 'PATCH' or request.method == 'POST':
         FORM_TABLE.insert_one(json_content)
 
-    return jsonify(success=True), 201
+    form = query_form(parm_dict, max_form_lst_len=1)[0]
+    return jsonify(form), 201
 
 
 def get_response(FormResponseID, remove_id=True):
