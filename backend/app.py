564d9316922601490c73d9fabed08b63f4583e47
Manu Chert <manu@tom.ac>
2020-11-21 15:39:22 -0500

    Added date filtering

--- backend/app.py
+++ backend/app.py
@@ -114,7 +114,9 @@ def process_query(form_lst, min_form_lst_len=None, max_form_lst_len=None, get_la
 
 def get_search_query(parm_dict, error_no_params=True):
     search_query = {}
-
+    start_time = None
+    end_time = None
+    to_date = lambda x: datetime.striptime(x.split("T"), "%d-%m-%d")
     for parm_key in parm_dict:
         if parm_dict[parm_key] is not None:
             if parm_key == 'FormName':
@@ -122,6 +124,15 @@ def get_search_query(parm_dict, error_no_params=True):
                     pass
                 else:
                     search_query[parm_key] = {'$regex': re.compile(parm_dict[parm_key], re.I)}
+            elif parm_key in ('StartTime', 'EndTime'):
+                if parm_key == 'StartTime':
+                    start_time = to_date(parm_dict[parm_key])
+                elif parm_key == 'EndTime':
+                    end_time = to_date(parm_dict[parm_key])
+                if start_time and end_time:
+                    search_query['CreateTime'] = {
+                            "$gte": start_time,
+                            "$lte": end_time}
             else:
                 search_query[parm_key] = parm_dict[parm_key]
 
